8280bc349451803894b99ec6def3bb961da909ee
闫松林 <songlinlin1994@163.com>
2019-04-24 11:44:41 +0800

    增加登录请求

--- base/run_test.py
+++ base/run_test.py
@@ -8,7 +8,13 @@ if __name__ == '__main__':
     # pytest.main(['-s', '-q','./TestCase'])
 
     # '--alluredir',
+
+
+
+
     pytest.main(['-s', '-q','--alluredir','./Report/xml/','./TestCase'])
+
+
 
     cmd = "allure generate ./Report/xml/ -o ./Report/html/ --clean"
     try:
